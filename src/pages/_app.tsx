16dc66cec4d6765c9b1bb46a49d191f13e524560
jina <jinkim2@student.42seoul.kr>
2024-10-09 16:48:37 +0900

    update: meta 정보 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>지메추</title>
+        <meta property="og:title" content="지메추" key="title" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
