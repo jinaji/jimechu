f5a5fad28dfb931a9ca4a4cb4741dd2ac504eaaa
jina <jinkim2@student.42seoul.kr>
2024-10-09 16:15:54 +0900

    update: 리스트 파일 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,158 +1,191 @@
-import localFont from "next/font/local";
-import {useEffect, useState} from "react";
+import localFont from 'next/font/local';
+import { useEffect, useState } from 'react';
+import {
+  DEFAULT_MENUS,
+  DESSERTS,
+  EMOJIS,
+  MAEJIRI_MENUS,
+  STONE_I_MENUS,
+} from '@/list/menus.list';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
-type MenuLabelType = '지메추' |'지디추'| '스메추' | '매메추'
+type MenuLabelType = '지메추' | '지디추' | '스메추' | '매메추';
 
 type MenuType = {
   label: MenuLabelType;
   description: string;
   menus: string[];
-}
+};
 
-const DEFAULT_MENUS = [
-    '한식', '양식', '중식', '아시안', '일식', '인도커리', '파스타', '피자', '야키토리', '삼겹살', '국밥', '해장국', '초밥', '라멘', '칼국수', '떡볶이', '순대국', '돼지곱창', '소곱창', '닭발',
-]
+const MENU_TYPE: MenuType[] = [
+  {
+    label: '지메추',
+    description: '지나의 메뉴 추천',
+    menus: DEFAULT_MENUS,
+  },
+  {
+    label: '지디추',
+    description: '지나의 디저트 추천',
+    menus: DESSERTS,
+  },
+  {
+    label: '스메추',
+    description: '스톤아이 저녁 메뉴 추천',
+    menus: STONE_I_MENUS,
+  },
+  {
+    label: '매메추',
+    description: '매지리 메뉴 추천',
+    menus: MAEJIRI_MENUS,
+  },
+];
 
-const DESSERTS = [
-    '퀸아망', '딸기케이크', '초코케이크', '크로와상','크로플','크루키','메론빵','아이스크림','몽블랑','빙수','홍콩와플'
-]
+const TypeSelect = ({
+  selectedMenuType,
+  onClickMenuLabel,
+}: {
+  selectedMenuType?: MenuType;
+  onClickMenuLabel: (label: MenuLabelType) => void;
+}) => {
+  const labels = MENU_TYPE.map((menu) => menu.label);
 
-const STONE_I_MENUS = [
-    '찜닭', '굽네', '피자', '순대국', '칼국수', '서브웨이', '버거킹', '보노보스', '야래향',
-]
+  if (!selectedMenuType) return null;
 
-const MAEJIRI_MENUS = [
-    '오리훈제', '고모네', '치밥', '학식', '편의점 도시락', '엽떡', '컵밥'
-]
-
-const EMOJIS = [
-    '🐷','🐰','🌝','🎉','✨','🐙','💥','🍎','🍒','🍑','🥐','🍗','🍔','🎂','🍪','🍩','❤️‍','🔥','🐹','🧚','🔥'
-]
-
-const MENU_TYPE : MenuType[] = [
-    {
-        label: '지메추',
-        description:'지나의 메뉴 추천',
-        menus:DEFAULT_MENUS,
-    },
-    {
-        label: '지디추',
-        description: '지나의 디저트 추천',
-        menus: DESSERTS
-    },
-    {
-        label: '스메추',
-        description: '스톤아이 저녁 메뉴 추천',
-        menus: STONE_I_MENUS
-
-    },
-    {
-        label: '매메추',
-        description: '매지리 메뉴 추천',
-        menus: MAEJIRI_MENUS
-    }
-]
-
-const TypeSelect = ({selectedMenuType, onClickMenuLabel} : {selectedMenuType?:MenuType;onClickMenuLabel: (label: MenuLabelType) => void}) => {
-    const labels = MENU_TYPE.map(menu => ( menu.label))
-
-    if (!selectedMenuType) return null;
-
-    return (
+  return (
     <div className={'flex flex-col gap-2 '}>
-        <div className={' gap-2 justify-center grid grid-cols-2 grid-rows-2 md:flex md:flex-row'}>
-            {labels.map((label, index) => (
-                <p key={index} onClick={() => onClickMenuLabel(label)} className={`w-fit text-center m-auto text:md font-bold cursor-pointer p-2 rounded-2xl ${selectedMenuType.label === label ? `bg-blue-200 dark:bg-emerald-900` : ''} md:text-2xl`}>
-                    {label}
-                </p>
-            ))}
-        </div>
-        <p className={'text-center'}>
-            {selectedMenuType.description}
-        </p>
+      <div
+        className={
+          ' gap-2 justify-center grid grid-cols-2 grid-rows-2 md:flex md:flex-row'
+        }
+      >
+        {labels.map((label, index) => (
+          <p
+            key={index}
+            onClick={() => onClickMenuLabel(label)}
+            className={`w-fit text-center m-auto text:md font-bold cursor-pointer p-2 rounded-2xl ${selectedMenuType.label === label ? `bg-blue-200 dark:bg-emerald-900` : ''} md:text-2xl`}
+          >
+            {label}
+          </p>
+        ))}
+      </div>
+      <p className={'text-center'}>{selectedMenuType.description}</p>
     </div>
-    )
-}
+  );
+};
 
-const SelectedMenus = ({selectedMenuType,selectedMenu,setSelectedMenu} : {selectedMenuType?: MenuType; selectedMenu: string; setSelectedMenu: (menu: string) => void; }) => {
+const SelectedMenus = ({
+  selectedMenuType,
+  selectedMenu,
+  setSelectedMenu,
+}: {
+  selectedMenuType?: MenuType;
+  selectedMenu: string;
+  setSelectedMenu: (menu: string) => void;
+}) => {
+  if (!selectedMenuType) return null;
 
-    if (!selectedMenuType) return null;
+  return (
+    <div className={'flex gap-12 flex-col justify-center overflow-hidden'}>
+      <div
+        key={selectedMenuType.label}
+        className={
+          'text-xl  font-bold pt-4 text-center flex gap-4 animate-infinite-scroll md:text-3xl md:pt-10'
+        }
+      >
+        {selectedMenuType.menus?.map((menu, index) => (
+          <div key={menu + index} className={'flex flex-shrink-0'}>
+            {menu}
+          </div>
+        ))}
+      </div>
 
-    return (
-        <div className={'flex gap-12 flex-col justify-center overflow-hidden'} >
-            <div
-                key={selectedMenuType.label}
-                className={'text-xl  font-bold pt-4 text-center flex gap-4 animate-infinite-scroll md:text-3xl md:pt-10' }>
-                  {selectedMenuType.menus?.map((menu, index) =>
-                      (<div key={menu + index} className={'flex flex-shrink-0'}>
-                          {menu}
-                      </div>))}
-            </div>
+      <div className={'flex flex-row align-middle justify-center gap-3'}>
+        <p
+          className={`text-lg font-medium text-center cursor-pointer md:text-2xl ${!selectedMenu ? 'animate-zzz' : ''}`}
+          onClick={() => {
+            const randomValues =
+              selectedMenuType?.menus[
+                Math.floor(Math.random() * selectedMenuType?.menus.length)
+              ];
+            setSelectedMenu(randomValues);
+          }}
+        >
+          추천받기
+        </p>
+      </div>
 
-            <div className={'flex flex-row align-middle justify-center gap-3'}>
-
-            <p className={`text-lg font-medium text-center cursor-pointer md:text-2xl ${!selectedMenu ? 'animate-zzz' : ''}`} onClick={()=>{
-                const randomValues = selectedMenuType?.menus[Math.floor(Math.random() * selectedMenuType?.menus.length)];
-                setSelectedMenu(randomValues)
-            }}>
-                추천받기
-            </p>
-
-            </div>
-
-
-            <p  className={'text-4xl break-keep font-extrabold text-center pb-10 md:pt-20 md:text-6xl'}>
-                {selectedMenu + (selectedMenu && EMOJIS[Math.floor(Math.random() * EMOJIS.length)])}
-            </p>
-        </div>
-    )
-
-}
+      <p
+        className={
+          'text-4xl break-keep font-extrabold text-center pb-10 md:pt-20 md:text-6xl'
+        }
+      >
+        {selectedMenu +
+          (selectedMenu && EMOJIS[Math.floor(Math.random() * EMOJIS.length)])}
+      </p>
+    </div>
+  );
+};
 
 export default function Home() {
-    const [selectedMenuLabel, setSelectedMenuLabel] = useState<MenuLabelType>('지메추')
-    const [selectedMenu, setSelectedMenu] = useState('');
-    const [selectedMenuType, setSelectedMenuType] = useState<MenuType | undefined>(undefined);
+  const [selectedMenuLabel, setSelectedMenuLabel] =
+    useState<MenuLabelType>('지메추');
+  const [selectedMenu, setSelectedMenu] = useState('');
+  const [selectedMenuType, setSelectedMenuType] = useState<
+    MenuType | undefined
+  >(undefined);
 
-    useEffect(() => {
-        setSelectedMenuType(MENU_TYPE.find(menu => menu.label === selectedMenuLabel))
-    },[selectedMenuLabel])
+  useEffect(() => {
+    setSelectedMenuType(
+      MENU_TYPE.find((menu) => menu.label === selectedMenuLabel),
+    );
+  }, [selectedMenuLabel]);
 
-    return (
-        <div
-            className={`${geistSans.variable} ${geistMono.variable} flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]`}
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} flex items-center justify-items-center min-h-screen 
+     p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]`}
     >
       <div className={'justify-center flex w-full'}>
-        <div className={'w-[100%] justify-center align-middle bg-white dark:bg-[#1f1f2a] md:w-[700px] rounded-[20px] shadow-2xl'}>
+        <div
+          className={
+            'w-[100%] justify-center align-middle bg-white dark:bg-[#1f1f2a] md:w-[700px] rounded-[20px] shadow-2xl'
+          }
+        >
           <div className={'p-10 h-fit   flex flex-col gap-5 md:h-[800px] '}>
-              <p className={'font-bold text-xl text-center pb-10 pt-2 break-keep text-wrap md:text-3xl'}>
-                  🐷 지나가 추천하는 오늘의 메뉴 🐷
-              </p>
+            <p
+              className={
+                'font-bold text-xl text-center pb-10 pt-2 break-keep text-wrap md:text-3xl'
+              }
+            >
+              🐷 지나가 추천하는 오늘의 메뉴 🐷
+            </p>
 
-          <TypeSelect
-          selectedMenuType={selectedMenuType}
-              onClickMenuLabel={(label: MenuLabelType)=> {
-                  setSelectedMenu('')
-                  setSelectedMenuLabel(label)
-              }}/>
+            <TypeSelect
+              selectedMenuType={selectedMenuType}
+              onClickMenuLabel={(label: MenuLabelType) => {
+                setSelectedMenu('');
+                setSelectedMenuLabel(label);
+              }}
+            />
 
-              <SelectedMenus selectedMenuType={selectedMenuType} selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu} />
+            <SelectedMenus
+              selectedMenuType={selectedMenuType}
+              selectedMenu={selectedMenu}
+              setSelectedMenu={setSelectedMenu}
+            />
           </div>
         </div>
       </div>
-
     </div>
   );
 }
-
